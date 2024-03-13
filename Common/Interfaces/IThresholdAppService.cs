98ca45ef8657aa28c4c7688002d4dfb59192145e
Mr.Chen <290668617@qq.com>
2024-03-13 23:09:13 +0800

    httpapi层

---
+++ Common/Interfaces/IThresholdAppService.cs
@@ -0,0 +1,13 @@
+﻿using Common.Entities;
+using Volo.Abp.DependencyInjection;
+
+namespace Common.Interfaces
+{
+    /// <summary>
+    /// 阈值管理
+    /// </summary>
+    public interface IThresholdAppService : IBaseNetAppService<Threshold>, ITransientDependency
+    {
+
+    }
+}
